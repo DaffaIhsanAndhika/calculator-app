92e867c9f49f47f964aa1ecb0e110b9fd489eec4
DaffaIhsanAndhika <daffaihsanandhika3@students.amikom.ac.id>
2024-03-06 17:01:56 +0700

    Melengkapi fungsi Main

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,12 @@ class Calculator
 {
     static void Main (string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil penambahan: (0) + (1) = (2)", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: (0) - (1) = (2)", a, b, Pengurangan(a, b));
+		
 		
 	}
 	
